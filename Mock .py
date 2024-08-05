b780ed665fcab0480c999bf943911fa5fe39272e
Monisha20044 <monishap542@gmail.com>
2024-08-05 14:53:46 +0530

    Create Mock .py

---
+++ Mock .py
@@ -0,0 +1,13 @@
+movies = {
+    "1": {"title": "The Shawshank Redemption", "genre": "Drama"},
+    "2": {"title": "The Godfather", "genre": "Crime"},
+    "3": {"title": "The Dark Knight", "genre": "Action"},
+    "4": {"title": "12 Angry Men", "genre": "Drama"},
+    "5": {"title": "Schindler's List", "genre": "Biography"},
+}
+
+user_ratings = {
+    "user1": {"1": 5, "2": 4, "3": 4},
+    "user2": {"2": 5, "3": 5, "4": 3},
+    "user3": {"1": 4, "4": 5, "5": 4},
+}
