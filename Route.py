d2ece6b98c869962a1e719030750b4f99f443b43
Monisha20044 <monishap542@gmail.com>
2024-08-05 14:54:52 +0530

    Create Route.py

---
+++ Route.py
@@ -0,0 +1,8 @@
+@app.route('/recommendations', methods=['GET'])
+def recommendations():
+    user_id = request.args.get('user_id')
+    if not user_id:
+        return jsonify({"error": "User ID is required"}), 400
+
+    recommendations = get_recommendations(user_id)
+    return jsonify(recommendations)
